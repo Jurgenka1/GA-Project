9ff17c29061a831841c3c4b6c2d9f0d865a232b2
Jurgenka1 <jurgen.ka@gmail.com>
2021-05-10 12:39:30 -0400

    working

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ img1.addEventListener('mouseover', () => {
     img1.style.backgroundColor = "red"
 })
 
-const img1p = document.querySelector('#img1p')
+// const img1p = document.querySelector('#img1p')
 
-const Image1 = document.querySelector('.Image1')
+// const Image1 = document.querySelector('.Image1')
 
-const Imagine1close = document.querySelector('#img1-close')
+// const Imagine1close = document.querySelector('#img1-close')
 
-img1p.addEventListener('click', () => {
-    menu.classList.add('menu-open')
-})
+// img1p.addEventListener('click', () => {
+//     menu.classList.add('menu-open')
+// })
 
-magine1close.addEventListener('click', () => {
-    menu.classList.remove('menu-open')
-})
+// magine1close.addEventListener('click', () => {
+//     menu.classList.remove('menu-open')
+// })
